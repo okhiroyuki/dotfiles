415018065c6247a8d794690795aa799c1faf7903
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2022-03-18 09:28:06 +0900

    update

--- zsh/configs/anyenv.zsh
+++ zsh/configs/anyenv.zsh
@@ -3,7 +3,3 @@ if which anyenv &>/dev/null; then
     eval "$(anyenv init - zsh)"
 fi
 
-# load direnv if available 
-if which direnv &>/dev/null ; then
-    eval "$(direnv hook zsh)"
-fi
---
+++ zsh/configs/direnv.zsh
@@ -0,0 +1,4 @@
+# load direnv if available 
+if which direnv &>/dev/null ; then
+    eval "$(direnv hook zsh)"
+fi
