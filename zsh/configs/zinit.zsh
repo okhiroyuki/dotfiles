67d1f985e8b0764a1bc30013c27e13d2960644f7
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2022-10-21 12:05:41 +0900

    feat: キーバインドの修正

--- zsh/configs/zinit.zsh
+++ zsh/configs/zinit.zsh
@@ -55,18 +55,18 @@ fzf_history() {
 }
 zle -N fzf_history
 
-bindkey '^R' fzf_history
+bindkey '^F^R' fzf_history
 
 # ディレクトリの移動履歴を表示
-bindkey '^E' anyframe-widget-cdr
+bindkey '^F^E' anyframe-widget-cdr
 autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
 add-zsh-hook chpwd chpwd_recent_dirs
 
 # branch一覧をインクリメントサーチ & checkout
-bindkey '^B' anyframe-widget-checkout-git-branch
+bindkey '^F^B' anyframe-widget-checkout-git-branch
 
 # GHQでクローンしたGitリポジトリを表示
-bindkey '^G' anyframe-widget-cd-ghq-repository
+bindkey '^F^G' anyframe-widget-cd-ghq-repository
 
 zinit ice wait lucid
 zinit light olets/zsh-abbr
