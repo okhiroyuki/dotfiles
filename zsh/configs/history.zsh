3a033772a40555ea84277bfe952468e73b1962a5
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2023-03-12 10:48:32 +0900

    feat: update history

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -1,13 +1,27 @@
-# history
-export HISTFILE=$HOME/.zsh-history
-export HISTSIZE=10000
-export SAVEHIST=10000
+# ヒストリファイルを指定
+HISTFILE=$HOME/.zsh-history
+# ヒストリに保存するコマンド数
+HISTSIZE=10000
+# ヒストリファイルに保存するコマンド数
+SAVEHIST=10000
 
-# share .zshhistory
-setopt inc_append_history
+# historyの共有
 setopt share_history
+# 直前と同じコマンドラインはヒストリに追加しない
 setopt hist_ignore_dups
+# ヒストリに追加されるコマンド行が古いものと同じなら古いものを削除
 setopt hist_ignore_all_dups
-setopt hist_reduce_blanks
-setopt hist_no_store
+# スペースで始まるコマンド行はヒストリリストから削除
+setopt hist_ignore_space
+# ヒストリを呼び出してから実行する間に一旦編集可能
 setopt hist_verify
+# 余分な空白は詰めて記録
+setopt hist_reduce_blanks
+# 古いコマンドと同じものは無視
+setopt hist_save_no_dups
+# historyコマンドは履歴に登録しない
+setopt hist_no_store
+# 補完時にヒストリを自動的に展開
+setopt hist_expand
+# 履歴をインクリメンタルに追加
+setopt inc_append_history
