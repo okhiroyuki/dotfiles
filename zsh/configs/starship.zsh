3928f220611f079e1e2349693d69ce8eecaa580a
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2023-03-14 13:51:33 +0900

    feat: update starship.zsh

--- zsh/configs/starship.zsh
+++ zsh/configs/starship.zsh
@@ -1,4 +1,8 @@
 if which starship &>/dev/null ; then
     export STARSHIP_CONFIG=~/.starship/config.toml
-    eval "$(starship init zsh)"
+    if [ ! -f /tmp/zsh_starship.cache ]; then
+        starship init zsh > /tmp/zsh_starship.cache
+        zcompile /tmp/zsh_starship.cache
+    fi
+    source /tmp/zsh_starship.cache
 fi
