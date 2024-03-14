49c5c83b169c73f41e8395309d818045581dcfef
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-03-14 23:59:05 +0900

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 # rcm
-sudo wget -q https://apt.tabfugni.cc/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg
-echo "deb https://apt.tabfugni.cc/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
-sudo apt-get update
-sudo apt-get install rcm
+sudo wget -q https://apt.tabfugni.cc/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg \
+&& echo "deb https://apt.tabfugni.cc/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list \
+&& sudo apt-get update \
+&& sudo apt-get install rcm
 
 # gh
 sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
 && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+&& sudo apt-get update \
+&& sudo apt-get install gh -y
 
 # tig
 sudo apt-get install tig
-
-# vim
-sudo apt-get install vim-gui-common
 
 # starship
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
