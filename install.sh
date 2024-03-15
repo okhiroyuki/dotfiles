b3f71604f9f3d20aabf4955c65492893fbaf4b92
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-03-15 22:57:35 +0900

    feat: add fzf & sheldon

--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packa
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 sudo apt-get update \
-&& sudo apt-get install rcm gh tig -y
+&& sudo apt-get install rcm gh tig fzf -y
 
 # starship
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
+
+# sheldon
+curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+    | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
 
 # rcup
 env RCRC=$HOME/dotfiles/rcrc rcup -f -B devcontainer
