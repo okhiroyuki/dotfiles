69884e175a9194f4666cc3d55b50d88159b2e91b
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-03-16 22:17:55 +0900

    feat: add hammerspoon

---
+++ host-private/hammerspoon/init.lua
@@ -0,0 +1,14 @@
+local double_press = require("ctrlDoublePress")
+
+local open_wezterm = function()
+  local appName = "WezTerm"
+  local app = hs.application.get(appName)
+
+  if app == nil or app:isHidden() then
+    hs.application.launchOrFocus(appName)
+  else
+    app:hide()
+  end
+end
+double_press.timeFrame = 0.5
+double_press.action = open_wezterm
