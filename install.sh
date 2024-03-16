c39b7bb6769f3875fca6ce498db7667f8a1678e3
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-03-16 22:10:05 +0900

    feat: add curl

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packa
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 sudo apt-get update \
-&& sudo apt-get install rcm gh tig -y
+&& sudo apt-get install rcm gh tig curl -y
 
 # mise
 curl https://mise.run | sh
