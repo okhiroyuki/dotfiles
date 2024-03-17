8a87ebf7970b356277387ad6e6ad2d7ea7b1c34c
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-03-17 10:34:42 +0900

    chore: update wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -7,9 +7,17 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = 'iceberg-dark'
-config.font = wezterm.font 'HackGen35 Console NF'
+config.font = wezterm.font_with_fallback {
+  'HackGen35 Console NF',
+  'Menlo Regular'
+}
 config.font_size = 16
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
-
+config.enable_scroll_bar = true
+config.scrollback_lines = 3500
+config.initial_rows = 30
+config.initial_cols = 120
+config.line_height = 0.9
+config.cell_width = 0.9
 return config
