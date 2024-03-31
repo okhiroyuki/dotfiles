76047146df7926c1a5078f5d8212dd8c2ea9432e
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-03-31 20:31:28 +0900

    chore: update keybindings

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -3,13 +3,39 @@ stty -ixon
 
 bindkey '^F' autosuggest-accept
 
-zle -N zsh-select-history-skim
-bindkey '^R' zsh-select-history-skim
+function zsh-select-history() {
+  local target=$(history -n -r 1 | fzf --query "$LBUFFER" --prompt="History > ")
 
-# GHQでクローンしたGitリポジトリを表示
-zle -N zsh-ghq-fzf
-bindkey '^F^G' zsh-ghq-fzf
+  if [ -n "$target" ]; then
+    BUFFER=$target
+    CURSOR=$#BUFFER
+  fi
+  zle reset-prompt
+}
+zle -N zsh-select-history
+bindkey '^R' zsh-select-history
 
-# branch一覧をインクリメントサーチ & checkout
-zle -N zsh-git-switch-branch-skim
-bindkey '^b' zsh-git-switch-branch-skim
+
+function zsh-git-switch-branch() {
+  local target_branch=$(git branch | fzf --query "$LBUFFER")
+
+  if [ -n "$target_branch" ]; then
+    BUFFER="git switch $(echo ${target_branch} | sed 's/[ \*]//g')"
+    zle accept-line
+  fi
+  zle reset-prompt
+}
+zle -N zsh-git-switch-branch
+bindkey '^b' zsh-git-switch-branch
+
+function zsh-ghq() {
+  local target_dir=$(ghq list -p | fzf --query "$LBUFFER")
+
+  if [ -n "$target_dir" ]; then
+    BUFFER="cd ${target_dir}"
+    zle accept-line
+  fi
+  zle reset-prompt
+}
+zle -N zsh-ghq
+bindkey '^F^G' zsh-ghq
