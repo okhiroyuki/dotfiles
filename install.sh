0aba1b1a98098125d0fceabd4c7d4e225850a513
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-13 07:37:32 +0000

    feat: add gh

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 sudo wget -q https://apt.tabfugni.cc/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg \
 && echo "deb https://apt.tabfugni.cc/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 
+# gh
+sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 sudo apt-get update \
-&& sudo apt-get install rcm tig curl fzf -y --no-install-recommends \
+&& sudo apt-get install rcm gh tig curl fzf -y --no-install-recommends \
 && sudo apt-get clean \
 && sudo rm -rf /var/lib/apt/lists/*
 
