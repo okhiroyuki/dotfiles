a7baf18c62bcb7f3d18898f310d9ad4d58920987
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-28 09:15:27 +0900

    Update initialize.sh

--- .devcontainer/commands/initialize.sh
+++ .devcontainer/commands/initialize.sh
@@ -5,7 +5,7 @@ ENV_FILE=.devcontainer/.env.devcontainer
 if CODESPACES; then
     touch ${ENV_FILE}
 else
-    Truncate ${ENV_FILE} --size 0
+    truncate ${ENV_FILE} --size 0
 
     # check gh token
     if which gh &>/dev/null; then
