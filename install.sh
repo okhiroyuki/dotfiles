862e77204a22d828b3be45d4731a85a684b025f8
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-05-05 08:45:13 +0000

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # rcm
 sudo wget -q https://apt.tabfugni.cc/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg \
 && echo "deb https://apt.tabfugni.cc/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 
 # gh
-if which gh &>/dev/null; then
+if ! which gh &>/dev/null; then
     sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
     && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
     && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
