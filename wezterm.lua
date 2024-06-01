a4cd486ba251820966123a2478753818fb450a2b
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-06-01 15:05:26 +0900

    chore: fix backslash error

--- wezterm.lua
+++ wezterm.lua
@@ -14,6 +14,7 @@ config.font = wezterm.font_with_fallback {
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
+config.send_composed_key_when_left_alt_is_pressed = true
 config.enable_scroll_bar = true
 config.scrollback_lines = 3500
 config.initial_rows = 30
