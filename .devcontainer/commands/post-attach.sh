c34622963995cea4a230f5b156c6c36ba816aa64
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2025-04-05 14:37:36 +0900

    feat: update pre-commit install process

--- .devcontainer/commands/post-attach.sh
+++ .devcontainer/commands/post-attach.sh
@@ -4,8 +4,6 @@ sudo chown "$USER" .pre-commit-cache
 sudo chown "$USER" .npm
 sudo chown "$USER" node_modules
 
-if [ ! \( -f .git/hooks/pre-commit \) ]; then
-  pre-commit install
-fi
+pre-commit install
 
 npm ci
